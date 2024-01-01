59c7a957b81942434354079ac3fec308dd839e8f
Aden Bagja Nurmawan <adenbagja46@gmail.com>
2024-01-01 17:10:16 +0700

    add

--- extract_data.sh
+++ extract_data.sh
@@ -1,25 +1,31 @@
-if [ "$(command -v curl)" ]; then
-        gcp_req='curl -s -f  -H "X-Google-Metadata-Request: True"'
-elif [ "$(command -v wget)" ]; then
-    gcp_req='wget -q -O - --header "X-Google-Metadata-Request: True"'
-else 
-    echo "Neither curl nor wget were found, I can't enumerate the metadata service :("
+#!/bin/bash
+
+print_3title() {
+    echo "=== $1 ==="
+}
+
+if command -v curl &> /dev/null; then
+    gcp_req='curl -s -f -H "Metadata-Flavor: Google"'
+elif command -v wget &> /dev/null; then
+    gcp_req='wget -q -O - --header "Metadata-Flavor: Google"'
+else
+    echo "Neither curl nor wget were found. Exiting."
+    exit 1
 fi
 
 echo ""
 print_3title "Startup-script"
-echo $(eval $gcp_req "http://metadata.google.internal/computeMetadata/v1/instance/attributes/startup-script")
+echo "$($gcp_req http://metadata.google.internal/computeMetadata/v1/instance/attributes/startup-script)"
 echo ""
 
 echo ""
 print_3title "Service Accounts"
-for sa in $(eval $gcp_req "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/"); do 
+for sa in $($gcp_req http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/); do
     echo "  Name: $sa"
-    echo "  Email: "$(eval $gcp_req "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/$sa/email")
-    echo "  Aliases: "$(eval $gcp_req "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/$sa/aliases")
-    echo "  Identity: "$(eval $gcp_req "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/$sa/identity")
-    echo "  Scopes: "$(eval $gcp_req "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/$sa/scopes") | sed -${E} "s,${GCP_GOOD_SCOPES},${SED_GREEN},g" | sed -${E} "s,${GCP_BAD_SCOPES},${SED_RED},g"
-    echo "  Token: "$(eval $gcp_req "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/$sa/token")
+    echo "  Email: $($gcp_req http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/$sa/email)"
+    echo "  Aliases: $($gcp_req http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/$sa/aliases)"
+    echo "  Identity: $($gcp_req http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/$sa/identity)"
+    echo "  Scopes: $($gcp_req http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/$sa/scopes)"
+    echo "  Token: $($gcp_req http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/$sa/token)"
     echo "  ==============  "
-    done
-fi
+done
